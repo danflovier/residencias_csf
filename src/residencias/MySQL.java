e607cf663f53d43469ced55066a85e979b5f2b3f
fernandoalcasan <fernandoalcasan@outlook.com>
2017-11-22 13:30:27 -0600

    Asistencias chidas

--- src/residencias/MySQL.java
+++ src/residencias/MySQL.java
@@ -18,7 +18,7 @@ public class MySQL {
     
     private static Connection connection;
 
-    final String port = "3307";
+    final String port = "3306";
     final String url = "jdbc:mysql://localhost:" + port + "/";
     final String user = "root";
     final String pass = "";
